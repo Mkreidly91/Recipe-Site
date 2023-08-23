acb8d84f8b3d5571436251cdb2c97fb2d32dacd3
Mostafa Kreidly <mkreidly91@gmail.com>
2023-08-23 13:44:34 +0300

    Backend: some tidying up

--- Backend/app/Http/Controllers/API/AuthController.php
+++ Backend/app/Http/Controllers/API/AuthController.php
@@ -21,6 +21,7 @@ class AuthController extends Controller
             'email' => 'required|string|email',
             'password' => 'required|string',
         ]);
+
         $credentials = $request->only('email', 'password');
         $token = Auth::attempt($credentials);
 
