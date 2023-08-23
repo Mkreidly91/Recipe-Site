ae02ce76e5b89541cb693bc6b9c6698c4f5fe734
Mostafa Kreidly <mkreidly91@gmail.com>
2023-08-23 13:42:03 +0300

    Backend added routes for both user and recipe controller functions

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,18 +1,25 @@
 <?php
 
+use App\Http\Controllers\RecipeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 
-Route::controller(AuthController::class)->group(function () {
-    Route::post('login', 'login');
-    Route::post('register', 'register');
-    Route::post('logout', 'logout');
-    Route::post('refresh', 'refresh');
-});
+
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('logout', [AuthController::class, 'logout']);
+Route::post('refresh', [AuthController::class, 'refresh']);
+
 
 Route::prefix('user')->group(function () {
-    Route::controller(UserController::class)->group(function () {
-        Route::get('getAll', "getAllRecipes");
-    });
+    Route::get('getAll', [UserController::class, 'getAllRecipes']);
+    Route::post('like', [UserController::class, 'likeRecipe']);
+    Route::post('comment', [UserController::class, 'comment']);
+});
+
+Route::prefix('recipe')->group(function () {
+    Route::get('getComments/{recipeId}', [RecipeController::class, 'getRecipeComments']);
+    Route::post('addRecipe', [RecipeController::class, 'addRecipe']);
 });
