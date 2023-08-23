5384b821861300c7887598f1aee2aa04fbfa0df2
Mostafa Kreidly <mkreidly91@gmail.com>
2023-08-23 14:02:50 +0300

    Frontend: Initialized vite project, removed unwanted boilerplate code

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <p className="text-red-200">hello</p>
+    </>
+  );
+}
+
+export default App;
