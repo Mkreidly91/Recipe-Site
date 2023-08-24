53495fbff3e5f3d4ffd45cf2a8bca4713f7f9930
Mostafa Kreidly <mkreidly91@gmail.com>
2023-08-24 22:31:08 +0300

    Frontend: added helper functions

---
+++ Frontend/src/helpers/helpers.js
@@ -0,0 +1,36 @@
+function toBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+}
+const replaceObjectById = (id, newObject, state) => {
+  const updatedObjects = state.map((obj) =>
+    obj._id === id && obj ? newObject : obj
+  );
+  return updatedObjects;
+};
+
+const getUser = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+  return user;
+};
+
+function getUnique(submissions) {
+  const uniqueSubmissionsMap = new Map();
+  submissions.forEach((submission) => {
+    uniqueSubmissionsMap.set(submission.task_id, submission);
+  });
+  const uniqueSubmissions = Array.from(uniqueSubmissionsMap.values());
+  return uniqueSubmissions;
+}
+
+function compareDates(taskA, taskB) {
+  const dateA = new Date(taskA.submission_date);
+  const dateB = new Date(taskB.submission_date);
+  return dateA - dateB;
+}
+
+export { toBase64, replaceObjectById, getUser, getUnique, compareDates };
