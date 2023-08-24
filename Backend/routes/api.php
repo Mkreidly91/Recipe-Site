0511a1d249245d4e2bbfbda99d986ffe562bbfb0
Mostafa Kreidly <mkreidly91@gmail.com>
2023-08-25 01:46:12 +0300

    Backend: Added search route

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -23,4 +23,5 @@ Route::prefix('recipe')->group(function () {
     Route::get('getComments/{recipeId}', [RecipeController::class, 'getRecipeComments']);
     Route::post('addRecipe', [RecipeController::class, 'addRecipe']);
     Route::get('getMeasurements', [RecipeController::class, 'getMeasurements']);
+    Route::get('search/{q}', [RecipeController::class, 'search']);
 });
