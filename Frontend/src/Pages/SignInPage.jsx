bff95f05bce9cfc9c7d3c8328b9cf8ae9a9ddafe
Mostafa Kreidly <mkreidly91@gmail.com>
2023-08-25 06:31:35 +0300

    Frontend: removed ugly color from signIn/signUp

--- Frontend/src/Pages/SignInPage.jsx
+++ Frontend/src/Pages/SignInPage.jsx
@@ -5,7 +5,7 @@ import SignInForm from '../components/Forms/SignInForm';
 import logo from '../assets/images/Tastebite.png';
 const SignInPage = () => {
   return (
-    <div className=" h-full flex justify-center items-center relative b-pink">
+    <div className=" h-full flex justify-center items-center relative ">
       <div className=" absolute top-0 header  w-full flex items-center justify-between py-10 px-10">
         <img src={logo} className="w-[150px]" alt="" />
         <div className="flex justify-end text-xs text-black ">
